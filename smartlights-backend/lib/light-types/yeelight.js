f07c89612297be74356e5713881d43b8c6a0e20f
Johannes Bittner <johannes.bittner@gmail.com>
2021-12-30 01:21:40 +0100

    resolve light hostnames by name, and add ipv4 resolve hack

--- smartlights-backend/lib/light-types/yeelight.js
+++ smartlights-backend/lib/light-types/yeelight.js
@@ -1,10 +1,36 @@
 const yeelight = require('yeelight.io');
+const dnsPromises = require('dns/promises');
 
 const timeoutMs = 500;
 
+let dnsCache = {};
+/**
+ * Hack: Get IPv4 from host.
+ * This function is needed because some Yeelights are not accessible via their
+ * IPv6 address, therefore we explicitly request an IPv4 address.
+ * Using yeelight.io's `Bulb` otherwise resolves to the IPv6 address, and fails when connecting.
+ *
+ * @param {string} hostname
+ */
+async function getIPv4(hostname) {
+    if (dnsCache[hostname]) {
+        return dnsCache[hostname];
+    }
+
+    const ipv4Addresses = await dnsPromises.resolve4(hostname);
+    if (ipv4Addresses.length != 0) {
+        dnsCache[hostname] = ipv4Addresses[0];
+        return ipv4Addresses[0];
+    } else {
+        return null;
+    }
+}
+
 async function getStatus(host) {
-    return new Promise((resolve, reject) => {
-        const bulb = new yeelight.Bulb(host);
+    return new Promise(async (resolve, reject) => {
+        const ipv4 = await getIPv4(host);
+        const bulb = new yeelight.Bulb(ipv4);
+
         bulb.on('connected', () => {
             bulb.getProps();
         });
@@ -35,20 +61,26 @@ async function getStatus(host) {
     });
 }
 
-function setPower(host, power) {
+async function setPower(host, power) {
+    const ipv4 = await getIPv4(host);
+
     if (power === 'on') {
-        yeelight.on(host);
+        yeelight.on(ipv4);
     } else {
-        yeelight.off(host);
+        yeelight.off(ipv4);
     }
 }
 
-function setColor(host, color) {
-    yeelight.color(host, color.r, color.g, color.b);
+async function setColor(host, color) {
+    const ipv4 = await getIPv4(host);
+
+    yeelight.color(ipv4, color.r, color.g, color.b);
 }
 
-function setBrightness(host, brightness) {
-    yeelight.brightness(host, brightness);
+async function setBrightness(host, brightness) {
+    const ipv4 = await getIPv4(host);
+
+    yeelight.brightness(ipv4, brightness);
 }
 
 module.exports = {
