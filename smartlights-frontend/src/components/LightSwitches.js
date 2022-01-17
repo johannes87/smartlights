311ec82b56c93ac740cc9f8d70542d5880bce13d
Johannes Bittner <johannes.bittner@gmail.com>
2022-01-17 22:24:54 +0100

    use jsx fragment to avoid needing a key for all-lights-switch
    use jsx fragment instead of an array

--- smartlights-frontend/src/components/LightSwitches.js
+++ smartlights-frontend/src/components/LightSwitches.js
@@ -53,28 +53,29 @@ class LightSwitches extends React.Component {
         <div className="Loader" />
       </div>
     );
-    const lightSwitches = [
-      <FormControlLabel
-        control={
-          <Switch
-            checked={this.areSomeAvailableLightsTurnedOn()}
-            onChange={this.handleAllLightsSwitchChange}
-          />
-        }
-        label="All lights"
-      />,
-      Object.entries(this.state.lightStatuses).map(([lightId, lightStatus]) => (
-        <SwitchAndColorPicker
-          key={lightId}
-          lightId={lightId}
-          label={lightStatus.name}
-          color={lightStatus.color}
-          power={lightStatus.power}
-          brightness={lightStatus.brightness}
-          onPowerChange={this.handleLightPowerChange}
+    const lightSwitches =
+      <>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={this.areSomeAvailableLightsTurnedOn()}
+              onChange={this.handleAllLightsSwitchChange}
+            />
+          }
+          label="All lights"
         />
-      )),
-    ];
+        {Object.entries(this.state.lightStatuses).map(([lightId, lightStatus]) => (
+          <SwitchAndColorPicker
+            key={lightId}
+            lightId={lightId}
+            label={lightStatus.name}
+            color={lightStatus.color}
+            power={lightStatus.power}
+            brightness={lightStatus.brightness}
+            onPowerChange={this.handleLightPowerChange}
+          />
+        ))}
+      </>
 
     return (
       <FormGroup className="LightSwitches">
