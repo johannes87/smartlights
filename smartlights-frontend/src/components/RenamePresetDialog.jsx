79e3a1ebce0fc2372c05697da69ca05c3f6593ff
Johannes Bittner <johannes.bittner@gmail.com>
2023-08-27 18:38:48 +0200

    autofocus on input, submit with enter

--- smartlights-frontend/src/components/RenamePresetDialog.jsx
+++ smartlights-frontend/src/components/RenamePresetDialog.jsx
@@ -7,7 +7,7 @@ import {
   Button,
   TextField,
 } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function RenamePresetDialog({
   presetName,
@@ -15,6 +15,20 @@ export default function RenamePresetDialog({
   onClose,
 }) {
   const [newPresetName, setNewPresetName] = useState('');
+
+  const formSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      if (
+        newPresetName.trim() !== '' &&
+        newPresetName.trim() !== presetName.trim()
+      ) {
+        onNewNameChosen(presetName, newPresetName.trim());
+      }
+    },
+    [onNewNameChosen, presetName, newPresetName]
+  );
 
   useEffect(() => {
     if (presetName) {
@@ -30,41 +44,34 @@ export default function RenamePresetDialog({
       aria-labelledby="rename-preset-dialog-title"
       aria-describedby="rename-preset-dialog-description"
     >
-      <DialogTitle id="rename-preset-dialog-title">
-        Enter a new name
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText id="rename-preset-dialog-description">
-          Enter a name, which will become the new name of the preset{' '}
-          <strong>{presetName}</strong>.
-        </DialogContentText>
-        <div className="textfield-container">
-          <TextField
-            label="New name for the preset"
-            variant="filled"
-            value={newPresetName}
-            onChange={(e) => setNewPresetName(e.target.value)}
-          />
-        </div>
-      </DialogContent>
-      <DialogActions>
-        <Button variant="outlined" onClick={onClose} autoFocus>
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            if (
-              newPresetName.trim() !== '' &&
-              newPresetName.trim() !== presetName.trim()
-            ) {
-              onNewNameChosen(presetName, newPresetName.trim());
-            }
-          }}
-        >
-          Rename Preset
-        </Button>
-      </DialogActions>
+      <form onSubmit={formSubmit}>
+        <DialogTitle id="rename-preset-dialog-title">
+          Enter a new name
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="rename-preset-dialog-description">
+            Enter a name, which will become the new name of the preset{' '}
+            <strong>{presetName}</strong>.
+          </DialogContentText>
+          <div className="textfield-container">
+            <TextField
+              autoFocus
+              label="New name for the preset"
+              variant="filled"
+              value={newPresetName}
+              onChange={(e) => setNewPresetName(e.target.value)}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button variant="contained" type="submit">
+            Rename Preset
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
