0638ef5aedaa371a6fb6c70ca5017465c1d114f0
Johannes Bittner <johannes.bittner@gmail.com>
2023-09-03 19:08:36 +0200

    React 18: update to createRoot

--- smartlights-frontend/src/index.jsx
+++ smartlights-frontend/src/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './styles/styles.scss';
 import store from './redux/store';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
