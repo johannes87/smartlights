179b6c218e5ac6c9b4279641185ce3155a95126f
Johannes Bittner <johannes.bittner@gmail.com>
2023-12-26 20:55:28 +0100

    nicer fill color

--- smartlights-frontend/src/components/lightswitches/BrightnessIndicator.jsx
+++ smartlights-frontend/src/components/lightswitches/BrightnessIndicator.jsx
@@ -2,8 +2,9 @@ export default function BrightnessIndicator({
   currentBrightness,
   onIndicatorClick,
 }) {
+  const fillColor = 'rgba(185, 185, 185, 0.52)';
   const filledBackground = {
-    background: `linear-gradient(to right, grey ${currentBrightness}%, transparent ${currentBrightness}%)`,
+    background: `linear-gradient(to right, ${fillColor} ${currentBrightness}%, transparent ${currentBrightness}%)`,
   };
   return (
     <button
